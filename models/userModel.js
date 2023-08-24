103311b3fef2d117858808afb58600f99ccba94e
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2023-08-25 00:00:48 +0530

    edited user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,19 +2,27 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    fullName: {
+    firstName: {
       type: String,
       required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    contactNumber: {
+      type: Number,
+      unique: true,
+      required: true,
     },
     emailId: {
       type: String,
       unique: true,
       required: true,
     },
-    contactNumber: {
-      type: Number,
-      unique: true,
-      required: true,
+    password: {
+      type: String,
+      requireed: true,
     }
   }
 )
