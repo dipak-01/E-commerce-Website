e69b06827e803b96507fed0f6c583563c10c2cea
dipak-01 <imt2022037@iiitm.ac.in>
2023-08-28 21:44:41 +0530

    updates

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -55,11 +55,43 @@ hideAllImages();
 showCurrentImage();
 var docWidth = document.documentElement.offsetWidth;
 
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
+[].forEach.call(document.querySelectorAll("*"), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
   }
-);
+});
+
+// Get the button element
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// Smoothly scroll to the top of the document when the button is clicked
+scrollToTopBtn.addEventListener("click", function() {
+  // Using behavior: "smooth" for smooth scrolling
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+const container = document.getElementById("product-container");
+// where "container" is the id of the container
+  container.addEventListener("wheel", function (e) {
+    if (e.deltaY > 0) {
+      container.scrollLeft += 100;
+      e.preventDefault();
+// prevenDefault() will help avoid worrisome 
+// inclusion of vertical scroll 
+    }
+    else {
+      container.scrollLeft -= 100;
+      e.preventDefault();
+    }
+  });
