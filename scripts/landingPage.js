de806ef36dadce1877b09d94b6223968c1f95ede
dipak-01 <imt2022037@iiitm.ac.in>
2023-08-29 22:43:16 +0530

    designs update

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -64,7 +64,7 @@ var docWidth = document.documentElement.offsetWidth;
 // Get the button element
 var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-window.onscroll = function() {
+window.onscroll = function () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollToTopBtn.style.display = "block";
   } else {
@@ -73,25 +73,93 @@ window.onscroll = function() {
 };
 
 // Smoothly scroll to the top of the document when the button is clicked
-scrollToTopBtn.addEventListener("click", function() {
+scrollToTopBtn.addEventListener("click", function () {
   // Using behavior: "smooth" for smooth scrolling
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 });
 
 const container = document.getElementById("product-container");
 // where "container" is the id of the container
-  container.addEventListener("wheel", function (e) {
-    if (e.deltaY > 0) {
-      container.scrollLeft += 100;
-      e.preventDefault();
-// prevenDefault() will help avoid worrisome 
-// inclusion of vertical scroll 
-    }
-    else {
-      container.scrollLeft -= 100;
-      e.preventDefault();
-    }
+container.addEventListener("wheel", function (e) {
+  if (e.deltaY > 0) {
+    container.scrollLeft += 100;
+    e.preventDefault();
+    // prevenDefault() will help avoid worrisome
+    // inclusion of vertical scroll
+  } else {
+    container.scrollLeft -= 100;
+    e.preventDefault();
+  }
+});
+// wishlist
+document.addEventListener("DOMContentLoaded", function () {
+  var colorButton = document.getElementById("colorButton");
+
+  colorButton.addEventListener("click", function () {
+    // Toggle the 'active' class on button click
+    colorButton.classList.toggle("active");
   });
+});
+
+// creating cards
+// <!-- Your HTML container where you want to display the product cards -->
+// <div id="productContainer"></div>
+
+// Function to create a product card HTML structure
+function createProductCard(product) {
+  return `
+    <div class="product">
+      <img src="${product.imgSrc1}" alt="${product.altText}" />
+      <img id="change" src="${product.imgSrc2}" alt="${product.altText}" />
+      <div class="descr">
+        <span>DUNK</span>
+        <h4>${product.productName}</h4>
+        <h5>${product.price}</h5>
+      </div>
+      <div class="cart">
+        <a href="#" title="Add to Wishlist"><i class="bx bx-heart wishlist"></i></a>
+        <a href="#" title="Add to cart"><i class="bx bxs-cart-add cart1"></i></a>
+      </div>
+    </div>`;
+}
+
+// Get the container element
+const productCardGenerator=(x)=>{
+  function createProductCard(product) {
+    return `
+      <div class="product">
+        <img src="${product.imageUrl1}" alt=" product img" />
+        <img id="change" src="${product.imageUrl2}" alt=" product img" />
+        <div class="descr">
+          <span>DUNK</span>
+          <h4>${product.title}</h4>
+          <h5>₹ ${product.price}</h5>
+        </div>
+        <div class="cart">
+          <a href="#" title="Add to Wishlist"><i class="bx bx-heart wishlist"></i></a>
+          <a href="#" title="Add to cart"><i class="bx bxs-cart-add cart1"></i></a>
+        </div>
+      </div>`;
+  }
+  fetch("http://localhost:3000/product")
+  .then((res) => res.json())
+  .then((data) => {
+    // Generate product cards and append them to the container
+    for (let i = 0; i < 35; i++) {
+      const productCard = createProductCard(data[i]);
+      x.insertAdjacentHTML("beforeend", productCard);
+    };
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
+// Get the container element
+let x = document.getElementById("productContainer1");
+productCardGenerator(x);
+  x = document.getElementById("productContainer2");
+productCardGenerator(x);
