137e8a8888a5084e098d4c7300512bc7ea7cecc3
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2023-09-01 00:37:15 +0530

    Frontend and Backend Separated

--- frontend/scripts/landingPage.js
+++ frontend/scripts/landingPage.js
@@ -1,8 +1,8 @@
 const imageUrls = [
-  "Images/models/model1 (1).webp",
-  "Images/models/model1 (2).webp",
-  "Images/models/model1 (3).webp",
-  "Images/models/model1 (4).webp",
+  "../Images/models/model1 (1).webp",
+  "../Images/models/model1 (2).webp",
+  "../Images/models/model1 (3).webp",
+  "../Images/models/model1 (4).webp",
 ];
 
 const imageElement = document.getElementById("changingImage");
