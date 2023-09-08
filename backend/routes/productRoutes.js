693ac51d9903ba315b108df33d75f98a667cc91e
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2023-09-09 00:41:20 +0530

    Update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,5 +6,6 @@ router.post("/add-product", productController.addProduct);
 router.get("/product/:productId", productController.getProduct);
 router.get("/explore-all", productController.getAllproduct);
 router.put("/product-review/:productId", productController.addReview);
+router.get("/search",productController.search);
 
 module.exports = router;
