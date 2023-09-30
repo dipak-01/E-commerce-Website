1b1e98ef91d891edd64f295f8f98f62d029fc0cf
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2023-09-30 13:58:53 +0530

    get reviews route added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ const productController = require("../controllers/productControllers");
 router.post("/add-product", productController.addProduct);
 router.get("/product/:productId", productController.getProduct);
 router.get("/explore-all", productController.getAllproduct);
+router.get("/product-review/:productId",productController.getReview);
 router.put("/product-review/:productId", productController.addReview);
 router.get("/search",productController.search);
 
